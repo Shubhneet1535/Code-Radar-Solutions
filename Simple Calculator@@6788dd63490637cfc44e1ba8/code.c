ff7026884148927d162aa3a2250027c0de27a5b5
Shubhneet1535 <shubhneet1535.becse24@chitkara.edu.in>
2025-02-06 17:38:13 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 int main() {
     int a,b;
+    char operator;
     scanf("%d %d",&a,&b);
-    if (a==1){
-        printf("Monday");
-    }
-    else if(a==2){
-        printf("Tuesday");
-    }
-    else if(a==3){
-        printf("Wednesday");
-    }
-    else if(a==4){
-        printf("Thrusday");
-    }
-    else{
-        printf("Invalid");
+    scanf("%c",&operator);
+    switch(operator){
+        case '+':
+             printf("%d+%d=%d",num1,num2,num1+num2);
+             break;
+        case '-':
+             printf("%d-%d=%d",num1,num2,num1-num2);
+             break;
+        case '*':
+             printf("%d*%d=%d",num1,num2,num1*num2);
+             break;
+        case '/':
+            if(num2!=0){
+                printf("%d/%d=%d",num1,num2,num1/num2);
+            } else{
+                printf("Error");
+            }  
+            break;   
+        default:
+           printf("Invalid month");
     }
     return 0;
+}
